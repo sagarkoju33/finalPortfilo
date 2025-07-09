084d4c54d053e65f8cc5650be920f33e3a7cc1f5
Sagar.koju <sagar.koju@mypay.com.np>
2025-07-09 10:03:20 +0545

    issue fix

--- lib/view/main/components/navigation_bar.dart
+++ lib/view/main/components/navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/view%20model/responsive.dart';
 import 'package:portfolio/view/intro/components/side_menu_button.dart';
@@ -12,21 +13,25 @@ class TopNavigationBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Spacer(),
           Padding(
             padding: const EdgeInsets.all(defaultPadding),
             child: !Responsive.isLargeMobile(context)
                 ? Image.asset('assets/images/triange_icon.png')
                 : MenuButton(onTap: () => Scaffold.of(context).openDrawer()),
           ),
+          Spacer(flex: 2),
           // if(Responsive.isLargeMobile(context)) MenuButton(),
-          const Spacer(flex: 2),
-          if (!Responsive.isLargeMobile(context)) const NavigationButtonList(),
-          const Spacer(flex: 2),
-          const ConnectButton(),
-          const Spacer(),
+          if (!Responsive.isLargeMobile(context)) NavigationButtonList(),
+          Spacer(),
+
+          Padding(
+            padding: const EdgeInsets.only(right: defaultPadding),
+            child: const ConnectButton(),
+          ),
+          // const Spacer(),
         ],
       ),
     );
--- lib/view/main/components/navigation_button_list.dart
+++ lib/view/main/components/navigation_button_list.dart
@@ -20,37 +20,41 @@ class _NavigationButtonListState extends State<NavigationButtonList> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60,
-      child: ListView.builder(
-        // shrinkWrap: true,
-        scrollDirection: Axis.horizontal,
-        itemCount: navigationButtons.length,
-        physics: const BouncingScrollPhysics(),
-        itemBuilder: (context, index) {
-          final e = navigationButtons[index];
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: GestureDetector(
-              onTap: () {
-                controller.animateToPage(
-                  index,
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.easeIn,
-                );
-              },
-              child: Center(
-                child: Text(
-                  e,
-                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+    return Expanded(
+      child: SizedBox(
+        height: 60,
+        width: double.infinity,
+
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          scrollDirection: Axis.horizontal,
+          itemCount: navigationButtons.length,
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (context, index) {
+            final e = navigationButtons[index];
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: GestureDetector(
+                onTap: () {
+                  controller.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeIn,
+                  );
+                },
+                child: Center(
+                  child: Text(
+                    e,
+                    style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
