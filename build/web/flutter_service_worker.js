8a9dcea72528f6bbca33caa39b01994dd2ab60d7
Sagar.koju <sagar.koju@mypay.com.np>
2025-07-28 17:58:55 +0545

    issue fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -50,7 +50,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4b89f4d147410bbe5957ad580a3783bc
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "9fb5704e719f93940bba93d69ebbd5cb",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "af91ef787908c1ecf354ce83491e295f",
+"flutter_bootstrap.js": "919775ed738de19ba17df104ead88e74",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
