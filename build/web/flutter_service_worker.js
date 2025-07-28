a3645d447cd0349f641152e2389381b8a1a79366
sagarkoju <sagarkoju5@gmail.com>
2025-07-28 20:10:59 +0545

    deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -50,7 +50,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4b89f4d147410bbe5957ad580a3783bc
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "9fb5704e719f93940bba93d69ebbd5cb",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "e590b078592b3232111613cbdd0e6085",
+"flutter_bootstrap.js": "7b5fc1fc7473aeb7ceaea8c61312eda6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
